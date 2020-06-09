cfbf847292122d2758f5094fcfecb22a12e0c75d
maksatosmonov <maksatosmonov@gmail.com>
2020-06-09 14:42:54 +0600

    3version

--- itblog/article/views.py
+++ itblog/article/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, HttpResponse
-from article.models import Article
+from article.models import Article, Author
 
 
 def homepage(request):
     articles = Article.objects.all()
-    return render(request, "article/homepage.html", {"articles": articles})
+    wind = Author.objects.get(id=1)
+    
+    return render(request, "article/homepage.html", 
+    {
+        "articles": articles,
+        "wind":wind
+        
+    })
 
 # Create your views here.
